2550bb5746c706a169f7061040df7e20b1c6cc96
NADA MILONE <nadamilone@gmail.com>
2022-03-02 18:32:20 -0500

    commit tree 2d47a1b0927cde3a5e3f307a8402c3fb61b83b60

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return renderBooks(data);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
+
+
+//renders book titles into the DOM by passing a JSON object to renderBooks()
